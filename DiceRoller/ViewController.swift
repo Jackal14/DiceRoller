0abf505d9e1bbb28425dba94e43cfbe34d83754d
jackal14 <agm19a@acu.edu>
2022-11-21 19:26:13 -0600

    basic dice image and button click

--- DiceRoller/ViewController.swift
+++ DiceRoller/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var diceImageView: UIImageView!
+    @IBOutlet var criticalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonGotPressed()
+    {
+        rollDice()
+    }
+    
+    func rollDice()
+    {
+        print("We rolling")
+        
+        let rolledNumber = Int.random(in: 1...20)
+        let imageName = "d\(rolledNumber)"
+        
+        diceImageView.image = UIImage(named: imageName)
+    }
+    
 }
 
